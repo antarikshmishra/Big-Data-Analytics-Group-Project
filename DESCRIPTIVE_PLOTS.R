4c30126444a8f56e85edfa906baf77db6ba85898
JoelRozencweig <joel.rozencweig@gmail.com>
2017-06-18 17:36:00 +0200

    Add files via upload

--- DESCRIPTIVE_PLOTS.R
+++ DESCRIPTIVE_PLOTS.R
@@ -1,8 +1,13 @@
 #Load packages
 install.packages("ggplot2")
 install.packages("psych")
+install.packages("scales")
+install.packages("gridExtra")
 library(psych)
 library(ggplot2)
+library(scales)
+library(gridExtra)
+require(grid)
 
 #Descriptive statistics
 summary(data)
@@ -10,19 +15,54 @@ describe(data[,1:21])
 
 #Generate plots for numerical variables:
 ##gnumplots function (automatically generates plots for multiple numerical variables)
-gnumplots<-function(plotsdata,varlist){
+gnumplots<-function(data,varlist){
+  plots.mtx<-matrix(list(),nrow=length(varlist),ncol=3)
   for(i in 1:length(varlist)){
-    print(ggplot(plotsdata,aes(x=get(varlist[[i]])))+
-            geom_histogram()+
-            xlab(varlist[[i]])+
-            ylab(NULL))
-    print(ggplot(plotsdata,aes(x=y,y=get(varlist[[i]])))+
-            geom_violin(aes(fill=y))+
-            geom_boxplot(width=0.1,outlier.alpha=0)+
-            scale_x_discrete(labels=c("No","Yes"))+
-            xlab("Has the client subscribed a term deposit?")+
-            ylab(varlist[[i]])+
-            theme(legend.position="none")
+    plots.mtx[[i,1]]<-ggplot(data,aes(x=get(varlist[[i]])))+
+      geom_histogram(bins = 30)+
+      xlab(varlist[[i]])
+    plots.mtx[[i,2]]<-ggplot(data,aes(x=get(varlist[[i]]),fill=y,colour=y))+
+      geom_density(alpha=0.25)+
+      xlab(varlist[[i]])+
+      scale_y_continuous(labels = percent)
+    quant.values<-quantile(x = data[,varlist[[i]]], probs = seq(0,1,0.1))
+    plotsrc<-data.frame(
+      quant=c(1:10,1:10),
+      y=rep(c("yes","no"),each=10),
+      count=vector(mode = "integer",length = 20),
+      freq=vector(mode = "double",length = 20))
+    for (j in 1:20) {
+      if (j %in% c(1,11)) {
+        plotsrc$count[j]<-sum(data$y==plotsrc$y[j] & data[,varlist[[i]]]<=quant.values[plotsrc$quant[j]+1])
+      } else {
+        plotsrc$count[j]<-sum(data$y==plotsrc$y[j] & data[,varlist[[i]]]<=quant.values[plotsrc$quant[j]+1])-
+          sum(data$y==plotsrc$y[j] & data[,varlist[[i]]]<=quant.values[plotsrc$quant[j-1]+1])
+      }
+    }
+    for (j in 1:20) {
+      if (sum(plotsrc$count[plotsrc$quant==plotsrc$quant[j]])>0) {
+        plotsrc$freq[j]<-plotsrc$count[j]/sum(plotsrc$count[plotsrc$quant==plotsrc$quant[j]])
+      } else {
+        plotsrc$freq[j]<-plotsrc$freq[j-1]
+      }
+    }
+    plots.mtx[[i,3]]<-ggplot(data=plotsrc,aes(x=quant,y=freq,fill=y))+
+      geom_bar(stat="identity")+
+      xlab(paste(varlist[[i]],"(quantiles)",sep = " "))+
+      ylab("frequency")+
+      scale_x_continuous(breaks = seq(1,10,1),labels=quant.values[2:11])+
+      scale_y_continuous(labels=percent)+
+      guides(fill=guide_legend(title="y"))
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(2, 2)))
+    print(plots.mtx[[i,1]],
+          vp=viewport(layout.pos.row = 1, layout.pos.col = 1)
+    )
+    print(plots.mtx[[i,2]],
+          vp=viewport(layout.pos.row = 1, layout.pos.col = 2)
+    )
+    print(plots.mtx[[i,3]],
+          vp=viewport(layout.pos.row = 2, layout.pos.col = 1:2)
     )
   }
 }
@@ -33,26 +73,23 @@ gnumplots(data,numvarlist)
 #Generate plots for categorical variables
 ##gcatplots function (automatically generates plots for multiple categorical variables)
 gcatplots<-function(data,varlist){
+  plots.mtx<-matrix(list(),nrow=length(varlist),ncol=3)
   for (i in 1:length(varlist)){
-    print(
-      ggplot(data,aes(x = get(varlist[[i]])))+
-        geom_bar()+
-        xlab(varlist[[i]])
-    )
+    plots.mtx[[i,1]]<-ggplot(data,aes(x = get(varlist[[i]])))+
+      geom_bar()+
+      xlab(varlist[[i]])
     cont<-as.data.frame(table(data[,varlist[[i]]],data$y))
     freqy<-cont[cont$Var2=="yes",3]/sum(cont[cont$Var2=="yes",3])
     freqn<-cont[cont$Var2=="no",3]/sum(cont[cont$Var2=="no",3])
     dir<-names(table(data[,varlist[[i]]]))
-    dfy<-data.frame(dir=dir,y=rep("Yes",length(dir)),freq=freqy)
-    dfn<-data.frame(dir=dir,y=rep("No",length(dir)),freq=freqn)
+    dfy<-data.frame(dir=dir,y=rep("yes",length(dir)),freq=freqy)
+    dfn<-data.frame(dir=dir,y=rep("no",length(dir)),freq=freqn)
     df<-rbind(dfy,dfn)
-    print(
-      ggplot(data=df,aes(x=y,y=freq,fill=dir))+
-        geom_bar(stat="identity")+
-        xlab("Has the client subscribed a term deposit?")+
-        ylab("Frequency")+
-        guides(fill=guide_legend(title=varlist[[i]]))
-    )
+    plots.mtx[[i,2]]<-ggplot(data=df,aes(x=y,y=freq,fill=dir))+
+      geom_bar(stat="identity")+
+      ylab("frequency")+
+      scale_y_continuous(labels = percent)+
+      guides(fill=guide_legend(title=varlist[[i]]))
     len<-nrow(cont)
     Freqk<-vector(mode="integer",length=len)
     for (k in 1:(len/2)){
@@ -60,15 +97,24 @@ gcatplots<-function(data,varlist){
       Freqk[k+len/2]<-cont[k+len/2,3]/(cont[k,3]+cont[k+len/2,3])
     }
     cont$Freq<-Freqk
-    print(
-      ggplot(data=cont,aes(x=Var1,y=Freq,fill=Var2))+
-        geom_bar(stat="identity")+
-        xlab(varlist[[i]])+
-        ylab("Frequency")+
-        guides(fill=guide_legend(title="y"))
+    plots.mtx[[i,3]]<-ggplot(data=cont,aes(x=Var1,y=Freq,fill=Var2))+
+      geom_bar(stat="identity")+
+      xlab(varlist[[i]])+
+      ylab("frequency")+
+      scale_y_continuous(labels = percent)+
+      guides(fill=guide_legend(title="y"))
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(2, 2)))
+    print(plots.mtx[[i,1]],
+          vp=viewport(layout.pos.row = 1, layout.pos.col = 1)
+    )
+    print(plots.mtx[[i,2]],
+          vp=viewport(layout.pos.row = 1, layout.pos.col = 2)
+    )
+    print(plots.mtx[[i,3]],
+          vp=viewport(layout.pos.row = 2, layout.pos.col = 1:2)
     )
   }
-  rm(cont,dfy,dfn,df,dir,Freq,freqn,freqy,Freqk,len)
 }
 ##Create the variable list and use gcatplots function 
 catvarlist<-list("marital","education","default","housing","loan","contact","month","day_of_week","poutcome")
